2b75cc182b5b1b06725dd734ab5e1c3eb967b38a
Raghu Radhakrishnan <rrkrish561@gmail.com>
2020-02-01 10:44:51 -0500

    added caller

---
+++ Caller/caller.go
@@ -0,0 +1,8 @@
+package Caller
+
+type Caller struct {
+	CallId    string `json:"callId"`
+	Location  string `json:"location"`
+	Situation string `json:"situation"`
+	Name      string `json:"Name"`
+}
