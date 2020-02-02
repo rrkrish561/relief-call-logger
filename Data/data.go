804c42d3edf9d3553cf64cfe871907d5032339ac
Raghu Radhakrishnan <rrkrish561@gmail.com>
2020-02-01 19:45:43 -0500

    rename

--- Data/data.go
+++ Data/data.go
@@ -1,8 +1,12 @@
-package Caller
+package Data
 
-type Caller struct {
+type Data struct {
 	CallId    string `json:"callId"`
 	Location  string `json:"location"`
 	Situation string `json:"situation"`
 	Name      string `json:"name"`
+}
+
+func (d Data) UpdateTable() error {
+	return nil
 }
